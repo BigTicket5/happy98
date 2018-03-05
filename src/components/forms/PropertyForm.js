ab2dcd4b3b06851b160e6f3b574d0da97a036a8a
Zhenzhong Mao <dkrose.random@gmail.com>
2018-03-06 00:23:49 +0800

    update 20180306

--- src/components/forms/PropertyForm.js
+++ src/components/forms/PropertyForm.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {Form,Input,Grid} from "semantic-ui-react";
 import PropTypes from 'prop-types';
-
+const options = [
+    { key: 'm', text: 'Male', value: 'male' },
+    { key: 'f', text: 'Female', value: 'female' },
+  ]
 class PropertyForm extends React.Component{
     constructor(){
         super();
@@ -17,16 +20,27 @@ class PropertyForm extends React.Component{
 		})
     };
     render(){
+        const { value } = this.state;
         return(
             <Grid columns={2} divided>
                 <Grid.Row>
-                    <Grid.Column>
-                        <div>
-                            <Input label='Owner' placeholder='mysite.com'/>
-                        </div>
-                        <div>
-                            <Input label='Address' placeholder='Franklin Street'/>
-                        </div>
+                    <Grid.Column  width={5}>
+                        <Form>
+                            <Form.Field inline>
+                                <label>Owner Name</label>
+                                <Input placeholder='Owner Name' />
+                            </Form.Field>
+                            <Form.Field inline>
+                                <label>Address</label>
+                                <Input placeholder='Address' />
+                            </Form.Field>
+                            <Form.Field inline>
+                                <label>Fee</label>
+                                <Input placeholder='Rent fee' />
+                                <label>Fee</label>
+                                <Input placeholder='Rent fee' />
+                            </Form.Field>
+                        </Form>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
